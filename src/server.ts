b1473d4a0c87c1fe2599f6478b11017265dae19e
Sergey Khlobystov <ts.s.khlobystov@gmail.com>
2018-10-20 14:53:19 +0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import * as Http from 'http';
+import * as express from 'express';
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3000))
+app.use('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.log(`answering... ${req.url}`);
+  res.send({ code: 420 });
+});
+
+export function start() {
+  const server = new Http.Server(app);
+  server.listen(app.get('port'));
+  server.on('listening', () => {
+    console.log(`listening on port ${app.get('port')}...`);
+  });
+}
